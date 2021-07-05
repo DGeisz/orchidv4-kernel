fff035029e3026020a6eec3104891025835f37fb
Danny Geisz <dannygeisz@berkeley.edu>
2021-07-05 08:40:09 -0600

    Simply getting some dependencies figured out

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! orchidv4-kernel is the kernel for creating, studying,
+//! and manipulating orchid structures.
+//!
+//! Refer to the [Orchid Manifesto](https://www.exfizzassist.com/posts/the-orchid-manifesto/)
+//! for a more complete description of the motivations behind Orchid
+
+pub mod orchid_io;
+pub mod curator;
+pub mod page;
---
+++ src/page/mod.rs
@@ -0,0 +1,3 @@
+//! A Page is the top-most container for actual orchid
+//! structures.  It corresponds to a single editor page
+//!
