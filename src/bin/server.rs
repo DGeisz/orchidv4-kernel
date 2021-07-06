1d8ca715c52d5514b42c9fe5a19eed7c3f6a86ea
Danny Geisz <dannygeisz@berkeley.edu>
2021-07-06 09:35:16 -0600

    Got rid of old nasty websocket server, added an official ws_kernel_io binary

--- src/bin/server.rs
+++
@@ -1,8 +0,0 @@
-use orchidv4_kernel::kernel_io::websocket_server::run_server;
-
-#[tokio::main]
-async fn main() {
-    env_logger::init();
-
-    run_server("127.0.0.1:7200").await;
-}
--- src/kernel_io/mod.rs
+++ src/kernel_io/mod.rs
@@ -1,6 +1,4 @@
 //! The Kernel IO module provides methods for interfacing with
 //! the Orchid kernel.
 
-pub mod websocket_server;
-
 pub mod ws_io;
