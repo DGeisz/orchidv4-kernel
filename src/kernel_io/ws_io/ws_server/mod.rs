658694cd05fb00910777c3a789c6ef3693f7b77c
Danny Geisz <dannygeisz@berkeley.edu>
2021-07-06 14:13:21 -0600

    Got the ws server test working.  Dope

--- src/kernel_io/ws_io/ws_server/mod.rs
+++ src/kernel_io/ws_io/ws_server/mod.rs
@@ -11,6 +11,7 @@ use crate::kernel_io::ws_io::ws_message_consumption_port::{
 use futures_util::{SinkExt, StreamExt};
 use log::{error, info, trace};
 use std::net::SocketAddr;
+use std::thread;
 use tokio::{
     net::{TcpListener, TcpStream},
     sync::{mpsc, mpsc::Sender, oneshot, watch, watch::Receiver},
@@ -20,7 +21,10 @@ use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
 #[cfg(test)]
 mod tests;
 
-pub fn run_server(addr: &'static str, consumption_port: &impl WsMessageConsumptionPort) {
+pub async fn run_server(
+    addr: &'static str,
+    consumption_port: impl WsMessageConsumptionPort + Send + 'static,
+) {
     /*
     First initialize the channels used for
     communication between tasks
@@ -28,96 +32,115 @@ pub fn run_server(addr: &'static str, consumption_port: &impl WsMessageConsumpti
     let (out_tx, out_rx) = watch::channel(String::new());
     let (in_tx, mut in_rx) = mpsc::channel::<String>(32);
 
+    /*
+    Create a oneshot used for terminating the process
+    */
+    let (terminate_tx, mut terminate_rx) = watch::channel(false);
+
     let _unused_clone = in_tx.clone();
+
+    /*
+    In a new thread, loop on blocking receives to consume
+    the next incoming ws messages.  Blocking_recv only returns
+    None once the channel is closed, hence the while let.
+    */
+    thread::spawn(move || {
+        while let Some(msg) = in_rx.blocking_recv() {
+            info!("Received in orchid loop message: {}", msg);
+
+            /*
+            Consume the message, and if the message produces an output
+            send the message out to all open ws connections watching out_rx
+            */
+            match consumption_port.consume_ws_message(msg) {
+                MessageConsumptionResponse::Message(output) => {
+                    info!("Sending the output to ws clients");
+                    /*
+                    We have some text we should send back to the clients
+                    */
+                    if let Err(e) = out_tx.send(output) {
+                        error!("Error sending consumption output to ws connections: {}", e);
+                    }
+                }
+                MessageConsumptionResponse::Terminate(output) => {
+                    /*
+                    Time to terminate the server, but let's send the
+                    final message first
+                    */
+                    info!("Just received a termination request {}", output);
+
+                    if let Err(e) = out_tx.send(output) {
+                        error!(
+                            "Error sending consumption termination output to ws connections: {}",
+                            e
+                        );
+                    }
+
+                    break;
+                }
+                MessageConsumptionResponse::None => (),
+            }
+        }
+
+        for i in 0..3 {
+            if let Err(e) = terminate_tx.send(true) {
+                error!("Error sending termination message try {}: {}", i + 1, e);
+            } else {
+                break;
+            }
+        }
+    });
 
     /*
     Create a multi-threaded runtime and spawn
     a background task to handle all ws connections
     */
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .spawn(async move {
-            let listener = TcpListener::bind(addr)
-                .await
-                .expect("Server was unable to connect");
 
-            info!("Listening on {}", addr);
+    tokio::spawn(async move {
+        let listener = TcpListener::bind(addr)
+            .await
+            .expect("Server was unable to connect");
 
-            /*
-            Loop through all new connections, handling each one
-            */
-            loop {
-                match listener.accept().await {
-                    Ok((stream, _)) => {
-                        info!("Got a new stream!");
-
-                        match stream.peer_addr() {
-                            Ok(peer) => {
-                                /*
-                                At this point we have all the info necessary to handle
-                                the new connection, so clone the necessary channels and
-                                spawn a new task to handle the connection
-                                */
-                                let in_tx_clone = in_tx.clone();
-                                let out_rx_clone = out_rx.clone();
-
-                                tokio::spawn(handle_connection(
-                                    stream,
-                                    peer,
-                                    in_tx_clone,
-                                    out_rx_clone,
-                                ));
-                            }
-                            Err(e) => error!("Unable to acquire socket peer address: {}", e),
-                        }
-                    }
-                    Err(e) => error!("Failed to accept new connection: {}", e),
-                }
-            }
-        });
-
-    /*
-    In the main thread, loop on blocking receives to consume
-    the next incoming ws messages.  Blocking_recv only returns
-    None once the channel is closed, hence the while let.
-    */
-    while let Some(msg) = in_rx.blocking_recv() {
-        trace!("Received message: {}", msg);
+        info!("Listening on {}", addr);
 
         /*
-        Consume the message, and if the message produces an output
-        send the message out to all open ws connections watching out_rx
+        Loop through all new connections, handling each one
         */
-        match consumption_port.consume_ws_message(msg) {
-            MessageConsumptionResponse::Message(output) => {
-                /*
-                We have some text we should send back to the clients
-                */
-                if let Err(e) = out_tx.send(output) {
-                    error!("Error sending consumption output to ws connections: {}", e);
-                }
-            }
-            MessageConsumptionResponse::Terminate(output) => {
-                /*
-                Time to terminate the server, but let's send the
-                final message first
-                */
-                if let Err(e) = out_tx.send(output) {
-                    error!(
-                        "Error sending consumption termination output to ws connections: {}",
-                        e
-                    );
-                }
+        loop {
+            match listener.accept().await {
+                Ok((stream, _)) => {
+                    info!("Got a new stream!");
 
-                break;
+                    match stream.peer_addr() {
+                        Ok(peer) => {
+                            /*
+                            At this point we have all the info necessary to handle
+                            the new connection, so clone the necessary channels and
+                            spawn a new task to handle the connection
+                            */
+                            let in_tx_clone = in_tx.clone();
+                            let out_rx_clone = out_rx.clone();
+
+                            tokio::spawn(handle_connection(
+                                stream,
+                                peer,
+                                in_tx_clone,
+                                out_rx_clone,
+                            ));
+                        }
+                        Err(e) => error!("Unable to acquire socket peer address: {}", e),
+                    }
+                }
+                Err(e) => error!("Failed to accept new connection: {}", e),
             }
-            MessageConsumptionResponse::None => (),
         }
-    }
+    });
 
-    info!("Shutting down the server");
+    /*
+    Wait on the message to terminate the process
+    */
+    terminate_rx.changed().await.unwrap();
+    info!("Ending the ws server");
 }
 
 pub async fn handle_connection(
@@ -200,10 +223,9 @@ pub async fn handle_connection(
                         Let's be sure the message is text
                         */
                         if let Message::Text(msg_content) = msg {
-                            trace!(
+                            info!(
                                 "Received message {} from ws connection on {}",
-                                msg_content,
-                                peer
+                                msg_content, peer
                             );
 
                             /*
@@ -213,8 +235,9 @@ pub async fn handle_connection(
                             for i in 0..3 {
                                 if let Err(e) = in_tx.send(msg_content.clone()).await {
                                     error!(
-                                        "Error sending ws msg content to \
+                                        "Error sending ws msg content {} to \
                                         message consumer from {} on try {}: {}",
+                                        msg_content.clone(),
                                         peer,
                                         i + 1,
                                         e
--- src/kernel_io/ws_io/ws_server/tests.rs
+++ src/kernel_io/ws_io/ws_server/tests.rs
@@ -13,8 +13,8 @@ use tokio_tungstenite::tungstenite::Message;
 
 /// This is a basic test of the server functionality.
 /// Spins up two websocket clients for communication
-#[test]
-fn multi_client_test() {
+#[tokio::test]
+async fn multi_client_test() {
     env_logger::init();
 
     let addr = "127.0.0.1:7200";
@@ -51,190 +51,187 @@ fn multi_client_test() {
     Create a tokio runtime for running the clients
     in their own thread
     */
-    // let runtime = tokio::runtime::Builder::new_multi_thread()
-    //     .worker_threads(1)
-    //     .enable_all()
-    //     .build()
-    //     .unwrap();
-    //
-    // let task = runtime.spawn(async move {
-    //     /*
-    //     Create url out of addr
-    //     */
-    //     let url = url::Url::parse(ws_addr).unwrap();
-    //
-    //     sleep(Duration::from_secs(2)).await;
-    //
-    //     /*
-    //     Create the first client, and split her up
-    //     */
-    //     info!("Creating first ws client");
-    //
-    //     let (ws1, _) = connect_async(url.clone())
-    //         .await
-    //         .expect("First client failed to connect");
-    //
-    //     let (mut ws1_write, mut ws1_read) = ws1.split();
-    //
-    //     info!("Creating second ws client");
-    //
-    //     /*
-    //     Same thing for second client
-    //     */
-    //     let (ws2, _) = connect_async(url)
-    //         .await
-    //         .expect("Second client failed to connect");
-    //
-    //     let (mut ws2_write, mut ws2_read) = ws2.split();
-    //
-    //     /*
-    //     Create a barrier to synchronize the flow of the test
-    //     */
-    //     let barrier = Arc::new(Barrier::new(4));
-    //
-    //     let bc1 = barrier.clone();
-    //     let bc2 = barrier.clone();
-    //     let bc3 = barrier.clone();
-    //     let bc4 = barrier.clone();
-    //
-    //     /*
-    //     Alright, let's handle reads and writes in their own processes.
-    //     Reads first
-    //     */
-    //     let read_handle = tokio::spawn(async move {
-    //         /*
-    //         Spawn separate tasks for different clients
-    //         */
-    //         let c1 = tokio::spawn(async move {
-    //             bc1.wait().await;
-    //
-    //             if let Message::Text(msg1) = ws1_read.next().await.unwrap().unwrap() {
-    //                 assert_eq!(msg1, res1_c1);
-    //             } else {
-    //                 panic!("Didn't receive text from client 1 first read");
-    //             }
-    //
-    //             bc1.wait().await;
-    //
-    //             if let Message::Text(msg2) = ws1_read.next().await.unwrap().unwrap() {
-    //                 assert_eq!(msg2, res2_c1);
-    //             } else {
-    //                 panic!("Didn't receive text from client 1 second read");
-    //             }
-    //
-    //             bc1.wait().await;
-    //
-    //             /*
-    //             No op occurs
-    //             */
-    //
-    //             bc1.wait().await;
-    //
-    //             if let Message::Text(msg3) = ws1_read.next().await.unwrap().unwrap() {
-    //                 assert_eq!(msg3, kill_res_c1);
-    //             } else {
-    //                 panic!("Didn't receive text from client 1 kill read");
-    //             }
-    //         });
-    //
-    //         let c2 = tokio::spawn(async move {
-    //             bc2.wait().await;
-    //
-    //             if let Message::Text(msg1) = ws2_read.next().await.unwrap().unwrap() {
-    //                 assert_eq!(msg1, res1_c2);
-    //             } else {
-    //                 panic!("Didn't receive text from client 2 first read");
-    //             }
-    //
-    //             bc2.wait().await;
-    //
-    //             if let Message::Text(msg2) = ws2_read.next().await.unwrap().unwrap() {
-    //                 assert_eq!(msg2, res2_c2);
-    //             } else {
-    //                 panic!("Didn't receive text from client 2 second read");
-    //             }
-    //
-    //             bc2.wait().await;
-    //
-    //             /*
-    //             No-op occurs
-    //             */
-    //
-    //             bc2.wait().await;
-    //
-    //             if let Message::Text(msg3) = ws2_read.next().await.unwrap().unwrap() {
-    //                 assert_eq!(msg3, kill_res_c2);
-    //             } else {
-    //                 panic!("Didn't receive text from client 2 kill read");
-    //             }
-    //         });
-    //
-    //         let (c1j, c2j) = tokio::join!(c1, c2);
-    //
-    //         c1j.unwrap();
-    //         c2j.unwrap();
-    //     });
-    //
-    //     /*
-    //     Now let's handle writes
-    //     */
-    //     let write_handle = tokio::spawn(async move {
-    //         let c1 = tokio::spawn(async move {
-    //             bc3.wait().await;
-    //
-    //             ws1_write.send(Message::Text(req1)).await.unwrap();
-    //
-    //             bc3.wait().await;
-    //
-    //             /*
-    //             Now the next message is sent
-    //             */
-    //             bc3.wait().await;
-    //
-    //             /*
-    //             Wait for the no-op
-    //             */
-    //
-    //             bc3.wait().await;
-    //
-    //             ws1_write.send(Message::Text(kill_req)).await.unwrap();
-    //         });
-    //
-    //         let c2 = tokio::spawn(async move {
-    //             bc4.wait().await;
-    //
-    //             /*
-    //             Now the first message is sent
-    //             */
-    //
-    //             bc4.wait().await;
-    //
-    //             ws2_write.send(Message::Text(req2)).await.unwrap();
-    //
-    //             bc4.wait().await;
-    //
-    //             ws2_write.send(Message::Text(no_op_req)).await.unwrap();
-    //
-    //             bc4.wait().await;
-    //
-    //             /*
-    //             Now the kill request is sent
-    //             */
-    //         });
-    //
-    //         let (c1j, c2j) = tokio::join!(c1, c2);
-    //
-    //         c1j.unwrap();
-    //         c2j.unwrap();
-    //     });
-    //
-    //     let (read, write) = tokio::join!(read_handle, write_handle);
-    //
-    //     read.unwrap();
-    //     write.unwrap();
-    // });
 
-    // runtime.block_on(task).unwrap();
+    let task = tokio::spawn(async move {
+        /*
+        Create url out of addr
+        */
+        let url = url::Url::parse(ws_addr).unwrap();
+
+        sleep(Duration::from_millis(10)).await;
+
+        /*
+        Create the first client, and split her up
+        */
+        info!("Creating first ws client");
+
+        let (ws1, _) = connect_async(url.clone())
+            .await
+            .expect("First client failed to connect");
+
+        let (mut ws1_write, mut ws1_read) = ws1.split();
+
+        info!("Creating second ws client");
+
+        /*
+        Same thing for second client
+        */
+        let (ws2, _) = connect_async(url)
+            .await
+            .expect("Second client failed to connect");
+
+        let (mut ws2_write, mut ws2_read) = ws2.split();
+
+        /*
+        Create a barrier to synchronize the flow of the test
+        */
+        let barrier = Arc::new(Barrier::new(4));
+
+        let bc1 = barrier.clone();
+        let bc2 = barrier.clone();
+        let bc3 = barrier.clone();
+        let bc4 = barrier.clone();
+
+        /*
+        Alright, let's handle reads and writes in their own processes.
+        Reads first
+        */
+        let read_handle = tokio::spawn(async move {
+            /*
+            Spawn separate tasks for different clients
+            */
+            let c1 = tokio::spawn(async move {
+                bc1.wait().await;
+
+                if let Message::Text(msg1) = ws1_read.next().await.unwrap().unwrap() {
+                    assert_eq!(msg1, res1_c1);
+                } else {
+                    panic!("Didn't receive text from client 1 first read");
+                }
+
+                info!("Made it past the first read assertion");
+
+                bc1.wait().await;
+
+                if let Message::Text(msg2) = ws1_read.next().await.unwrap().unwrap() {
+                    assert_eq!(msg2, res2_c1);
+                } else {
+                    panic!("Didn't receive text from client 1 second read");
+                }
+
+                bc1.wait().await;
+
+                /*
+                No op occurs
+                */
+
+                bc1.wait().await;
+
+                if let Message::Text(msg3) = ws1_read.next().await.unwrap().unwrap() {
+                    assert_eq!(msg3, kill_res_c1);
+                } else {
+                    panic!("Didn't receive text from client 1 kill read");
+                }
+            });
+
+            let c2 = tokio::spawn(async move {
+                bc2.wait().await;
+
+                if let Message::Text(msg1) = ws2_read.next().await.unwrap().unwrap() {
+                    assert_eq!(msg1, res1_c2);
+                } else {
+                    panic!("Didn't receive text from client 2 first read");
+                }
+
+                bc2.wait().await;
+
+                if let Message::Text(msg2) = ws2_read.next().await.unwrap().unwrap() {
+                    assert_eq!(msg2, res2_c2);
+                } else {
+                    panic!("Didn't receive text from client 2 second read");
+                }
+
+                bc2.wait().await;
+
+                /*
+                No-op occurs
+                */
+
+                bc2.wait().await;
+
+                if let Message::Text(msg3) = ws2_read.next().await.unwrap().unwrap() {
+                    assert_eq!(msg3, kill_res_c2);
+                } else {
+                    panic!("Didn't receive text from client 2 kill read");
+                }
+            });
+
+            let (c1j, c2j) = tokio::join!(c1, c2);
+
+            c1j.unwrap();
+            c2j.unwrap();
+        });
+
+        /*
+        Now let's handle writes
+        */
+        let write_handle = tokio::spawn(async move {
+            let c1 = tokio::spawn(async move {
+                bc3.wait().await;
+
+                ws1_write.send(Message::Text(req1)).await.unwrap();
+
+                bc3.wait().await;
+
+                /*
+                Now the next message is sent
+                */
+                bc3.wait().await;
+
+                /*
+                Wait for the no-op
+                */
+
+                bc3.wait().await;
+
+                sleep(Duration::from_millis(10)).await;
+
+                ws1_write.send(Message::Text(kill_req)).await.unwrap();
+            });
+
+            let c2 = tokio::spawn(async move {
+                bc4.wait().await;
+
+                /*
+                Now the first message is sent
+                */
+
+                bc4.wait().await;
+
+                ws2_write.send(Message::Text(req2)).await.unwrap();
+
+                bc4.wait().await;
+
+                ws2_write.send(Message::Text(no_op_req)).await.unwrap();
+
+                bc4.wait().await;
+
+                /*
+                Now the kill request is sent
+                */
+            });
+
+            let (c1j, c2j) = tokio::join!(c1, c2);
+
+            c1j.unwrap();
+            c2j.unwrap();
+        });
+
+        let (read, write) = tokio::join!(read_handle, write_handle);
+
+        read.unwrap();
+        write.unwrap();
+    });
 
     let mut mock = MockWsMessageConsumptionPort::new();
 
@@ -258,5 +255,7 @@ fn multi_client_test() {
         .times(1)
         .return_const(MessageConsumptionResponse::Terminate(kill_res));
 
-    run_server(addr, &mock);
+    let (_, t) = tokio::join!(run_server(addr, mock), task);
+
+    t.unwrap()
 }
